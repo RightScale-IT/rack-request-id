0771a0513b925beb4773de7f6f9e3a1c34af2bd1
Ben DeArmond <ben@rightscale.com>
2014-04-02 12:37:03 -0700

    enforce safety of passed-in request id
    https://github.com/rails/rails/blob/4-1-stable/actionpack/lib/action_dispatch/middleware/request_id.rb

--- lib/rack/request_id.rb
+++ lib/rack/request_id.rb
@@ -7,18 +7,29 @@ module Rack
     end
 
     def call(env)
-      request_id = env['HTTP_X_REQUEST_ID'] || SecureRandom.uuid
-
-      # Used by some things
+      request_id =  external_request_id(env) || internal_request_id
+      
       Thread.current[:request_id] = request_id
 
-      # Used by others
       Thread.current['action_dispatch.request_id'] = request_id
 
-      status, headers, body = @app.call(env)
-      headers['X-Request-Id'] ||= request_id
+      env["action_dispatch.request_id"] = request_id
+
       env['HTTP_X_REQUEST_ID'] = request_id
+      
       @app.call(env).tap { |_status, headers, _body| headers["X-Request-Id"] = request_id }
     end
+    
+    private
+      def external_request_id(env)
+        if request_id = env["HTTP_X_REQUEST_ID"].presence
+          request_id.gsub(/[^\w\-]/, "").first(255)
+        end
+      end
+
+      def internal_request_id
+        SecureRandom.uuid
+      end
+      
   end
 end
